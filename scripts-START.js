44f542883188fbe866295ea612246a332fc45bba
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-11-20 16:40:37 -0500

    handled endtime

--- scripts-START.js
+++ scripts-START.js
@@ -1,8 +1,12 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now(); // Use milliseconds
   const then = now + seconds * 1000 // multiply 1000 to convert seconds into milliseconds
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000) //Can't use now again because that captured the start time and cant access current time
@@ -11,6 +15,23 @@ function timer(seconds) {
       clearInterval(countdown);
     }
     //display it
-    console.log(secondsLeft);
+    displayTimeLeft(secondsLeft);
   }, 1000)
 };
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  // updates tab to read time left
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
